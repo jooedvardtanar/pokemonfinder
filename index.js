b70bc8896d251c8ce119f3eeeede976389ae3b3e
jooedvardtanar <joo.edvard@tanar.szamalk-szalezi.hu>
2025-05-10 11:02:48 +0200

    finish first version

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const OPTIONS = {
+  api: "https://pokeapi.co/api/v2/pokemon/",
+};
+
 const components = {
-    button: 'search-button',
-    input: 'search-input',
-    container: 'pokemon-container'
+  button: "search-button",
+  input: "search-input",
+  container: "pokemon-container",
+};
+
+document
+  .getElementById(components.button)
+  .addEventListener("click", async () => {
+    createPokemonImage(await getPokemonByName(
+        document.getElementById(components.input).value
+      ));
+  });
+
+async function getPokemonByName(pokemonName) {
+  return (await fetch(`${OPTIONS.api}${pokemonName}`)).json()
 }
 
-document.getElementById(components.button).addEventListener('click',() => {
-    alert('Keresünk!')
-})
+function createPokemonImage(data) {
+  document.getElementById(components.container).append(
+    Object.assign(document.createElement("img"), {
+      src: data.sprites.front_default,
+    })
+  );
+}
