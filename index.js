a72860ac5ef0cef750b09c10248339bf7651a17c
jooedvardtanar <joo.edvard@tanar.szamalk-szalezi.hu>
2025-05-10 11:10:31 +0200

    finish first version

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// Házi feladat:
+// https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
+// A fenti végpontot átalakítva képesek legyünk lekérni 0-9999-ig 
+// pokémonokat. Minden lekért pokemon képét jelenítsük meg!
+// A megejelenítés egy 5x5-ös gridben végezd el!
+// Használj css-t a grid kialakításához
+
 const OPTIONS = {
   api: "https://pokeapi.co/api/v2/pokemon/",
+  api2: "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
 };
 
 const components = {
